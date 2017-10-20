f905f51b40152a7a3b0fad23c1377821d48a3b17
Daniel McFadden <dmcfaddengalway@gmail.com>
2017-10-20 15:35:54 +0100

    revamp

--- quoteRotator.js
+++ quoteRotator.js
@@ -1,33 +1,38 @@
 $(document).ready(function() {
 
     var quoteNum;
+    var quote;
+    var author;
     var backgroundChoice;
-    var colors = ['#023C40', '#DCF763', '#95A472', '#C96480', '#8FD694', '#3A015C', '#4F0147', '#5C0029', '#8E0045', '#9E0031', '#8EB8E5', '#0D00A4', '#F71735', '#230C33', '#B27C66'];
+    var colors = ['#023C40', '#95A472', '#C96480', '#8FD694', '#3A015C', '#4F0147', '#5C0029', '#8E0045', '#9E0031', '#8EB8E5', '#0D00A4', '#F71735', '#230C33', '#B27C66'];
 
-    quoteNum = Math.floor(Math.random() * 69);
-    backgroundChoice = Math.floor(Math.random() * colors.length);
+    function getQuote() {
 
-    $("body").css("background-color", colors[backgroundChoice]);
-    $(".newQuoteButton").css("background-color", colors[backgroundChoice]);
+        quoteNum = Math.floor(Math.random() * 69);
+        backgroundChoice = Math.floor(Math.random() * colors.length);
 
-    $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/", function(val) {
-            console.log(val);
-
-            $(".quote").html(val[quoteNum].quote.slice(0, val[quoteNum].quote.length));
-            $(".author").html(val[quoteNum].author.slice(0, val[quoteNum].author.length));
-        }
-    );
-
-    $('.newQuoteButton').on('click', function(e) {
+        $("body").css("background-color", colors[backgroundChoice]);
+        $(".newQuoteButton").css("background-color", colors[backgroundChoice]);
 
         $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/", function(val) {
-                console.log(val);
+                //console.log(val);
 
                 $(".quote").html(val[quoteNum].quote.slice(0, val[quoteNum].quote.length));
                 $(".author").html(val[quoteNum].author.slice(0, val[quoteNum].author.length));
-            }
-        );
 
-    });
+        });
+    }
+
+    function shareQuote() {
+
+        $(".fa-twitter-square").on("click", function(e) {
+            window.openURL("https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='" + encodeURIComponent('"' + quote + '" ' + author));
+        });
+    }
+
+    getQuote();
+    shareQuote();
+
+    $(".newQuoteButton").on("click", getQuote);
 
 });
